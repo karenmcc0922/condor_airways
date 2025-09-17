aca00a48fb63c4562127fc33c7237499f09d207f
karenmcc0922 <karenmanuela.cardona@utp.edu.co>
2025-09-16 19:58:20 -0500

    Delete condor_airways directory

--- condor_airways/aerolinea/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("buscar-vuelos/", views.buscar_vuelos, name="buscar_vuelos"),
-    path("reservar-vuelo/<int:vuelo_id>/", views.reservar_vuelo, name="reservar_vuelo"),
-    path("comprar-vuelo/<int:reserva_id>/", views.comprar_vuelo, name="comprar_vuelo"),
-    path("checkin-vuelo/<int:compra_id>/", views.checkin_vuelo, name="checkin_vuelo"),
-]
